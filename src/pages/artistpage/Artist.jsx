26750f75c6094134d2deab06ba6348efa9c5c205
yunjioh <115560056+yunjioh@users.noreply.github.com>
2026-01-19 18:02:35 +0900

    artistlive

--- src/pages/artistpage/Artist.jsx
+++ src/pages/artistpage/Artist.jsx
@@ -3,6 +3,8 @@ import "./Artist.css";
 import ArtistBanner from "./sections/ArtistBanner";
 import ShopImportant from "../../components/shopcomp/ShopImportant";
 import SectionTitle from "../../components/homecomp/SectionTitle";
+import AboutUs from "./sections/AboutUs";
+import ArtistLive from "./sections/ArtistLive";
 
 const Artist = () => {
   return (
@@ -10,6 +12,32 @@ const Artist = () => {
       <ArtistBanner />
       <ShopImportant />
       <SectionTitle title="About US" showMore={true} useNicknameTitle={false} />
+      <AboutUs />
+      <SectionTitle
+        title="From StelLive"
+        useNicknameTitle={false}
+        moreElement={
+          <img
+            src="/img/reset.svg"
+            alt="refresh"
+            style={{ width: "24px", height: "24px" }}
+          />
+        }
+        onMoreClick={() => console.log("새로고침 클릭!")}
+      />
+      <ArtistLive />
+      <SectionTitle
+        title="To. StelLive"
+        useNicknameTitle={false}
+        moreElement={
+          <img
+            src="/img/reset.svg"
+            alt="refresh"
+            style={{ width: "24px", height: "24px" }}
+          />
+        }
+        onMoreClick={() => console.log("새로고침 클릭!")}
+      />
     </>
   );
 };
