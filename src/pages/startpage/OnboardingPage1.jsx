bfde7099d082861c2a900515249a7f2223243e5a
Yeeun <yen002506@gmail.com>
2026-01-19 16:19:16 +0900

    startpage-onboarding1 addd

--- src/pages/startpage/OnboardingPage1.jsx
+++ src/pages/startpage/OnboardingPage1.jsx
@@ -7,16 +7,19 @@ function OnboardingPage1() {
 
   return (
     <div className="onboarding-1">
+      <div className="onboarding-text">
+        <h2 className="title">만나서 반가워요!</h2>
+        <p className="desc">
+          당신을 도와줄 <br />
+          덕질메이트 <span className="point-color">튜비</span>에요!
+        </p>
+      </div>
+
       <img
         className="onboarding-image"
-        src="/img/onboarding-1.svg"
+        src="/img/onboarding-1.png"
         alt="onboarding-1"
       />
-
-      <div className="onboarding-text">
-        <h2 className="title">만나서 반가워요</h2>
-        <p className="desc">당신을 도와줄 덕질 메이트 튜비예요.</p>
-      </div>
     </div>
   );
 }
